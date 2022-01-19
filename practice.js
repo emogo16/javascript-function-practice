d70b2c2098b70d70af5002fe9418a7808e9834e9
Hannah McDowell <hannah.mcdowell1@gmail.com>
2022-01-18 20:57:25 -0800

    Add notes about functions from Epicodus.

---
+++ practice.js
@@ -0,0 +1,85 @@
+//    ------- Functions Practice --------
+// let test = alert("Are you sure?");
+// console.log(test);
+
+//alert("WARNING: Danger is imminent")
+
+//let caution = confirm("Are you sure you want to push the red button!?!?");
+//console.log(caution);
+
+// let que = prompt("Why did you push the red button?");
+// console.log (que.toUpperCase());
+// console.log (que.charAt(6));
+// console.log (que.slice(4));
+
+// let response = prompt("What's your dream vacation spot?");
+// response = response.repeat(3);
+// response = response.replaceAll('a', 'e').replaceAll('A', 'E');
+// console.log(response);
+
+// const inputAge = prompt("Enter your age:");
+// const age = parseInt(inputAge);
+// console.log(age + age);
+
+// const num = parseInt("5");
+// console.log(typeof num);
+
+// const pi = "3.74";
+// console.log(parseInt(pi));
+
+// function sayHi() {
+//   alert('Hello from Epicodus!');
+// }
+// sayHi();
+
+// function saySomething(whatToSay) {
+//   alert(whatToSay);
+// }
+// saySomething("hello!");
+
+// function add(number1, number2) {
+//   return number1 + number2;
+// }
+// console.log(add(3,5));
+
+// const add = function(number1, number2)
+//   {return number1 + number2;
+//   };
+//   console.log(add(3, 5));
+
+// function saySomething(whatToSay){
+//   alert(whatToSay);
+// }
+
+// function add(number1, number2){
+//   return number1 + number2;
+// }
+
+// const result = add(3, 5);
+// const outputText = "The sum is " + result + ".";
+// saySomething("The sum is " + add(3,5) + ".");
+
+//console.log("text".toUpperCase().concat(" other text"));
+
+//    ------- Writing Functions --------
+
+// input: name
+// function will call the alert function with a greeting
+// function greet(name){
+//  alert("Hello, " + name + "!");
+// }
+
+// function call
+//greet("Rian");
+
+// function subtract(number1, number2 ,number3){
+// return number1 - number2 - number3;
+// }
+// console.log(subtract(10, 900, 5));
+
+
+// function multiplyTwo( number1, number2 ){
+//   return number1 * number2;
+// }
+
+// console.log(multiplyTwo(3, 5));
